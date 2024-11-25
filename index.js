f2f232f5e6220c74d8bdb16a4479f4eaf96a330b
Yohan Maury <mauryyohan1@gmail.com>
2024-11-25 13:36:04 +0100

    Ultimate version with bonus included

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
 import { prompt } from './prompt.js';
 
-// 🦁 Guess the number
-const targetNumber = Math.floor(Math.random() * 100);
-let attempts = 0;
-let userNumber;
+// 🦁 Guess the number™
+const min = 0;
+const max = 100;
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // Fonction qui demande la saisie d'un nombre à l'utilisateur
-function guessNumberToUser() {
-    userNumber = prompt("Saisir un nombre : ");
-    while (userNumber < 0 || userNumber > 100 || isNaN(userNumber)) {
+const guessNumberToUser = () => {
+    let userNumber = prompt("Saisir un nombre : ");
+    while (userNumber < 0 || userNumber > 100 || isNaN(userNumber) || userNumber === "") {
         console.log("Merci d'entrer un nombre valide entre 0 and 100");
         userNumber = guessNumberToUser();
     }
     return userNumber;
 }
 
-// Fonction qui compare le nombre de l'utilisateur avec le nombre mystère
-function checkTheNumber(userNumber, targetNumber) {
-    if (userNumber < targetNumber) {
-        console.log("C'est plus !");
-    } else if (userNumber > targetNumber) {
-        console.log("C'est moins !");
+const playAgain = () => {
+    const replay = prompt("Enter 'yes' to replay or 'no' to quit \n");
+    if (replay.toLowerCase() === 'yes') {
+        main();
+    } else if (replay.toLowerCase() === 'no')  {
+        console.log("Goodbye !");
+        process.exit(0);
     } else {
-        console.log(`Bravo, vous avez trouvé le nombre mystère en ${attempts} essai(s)`);
+        console.log("Please enter a valid answer");
+        playAgain();
     }
 }
 
-// Coeur du programme, la boucle du jeu
-while (userNumber !== targetNumber) {
-    userNumber = guessNumberToUser();
-    attempts++;
-    checkTheNumber(userNumber, targetNumber);
+function endGame() {
+    console.log("Do you want to replay once ? ")
+    playAgain();
 }
+
+// Coeur du programme, la boucle du jeu
+function main() {
+    const targetNumber = getRandomNumber(min, max);
+    console.log("\n");
+    let attempts = 0;
+    let userNumber;
+
+    while (userNumber !== targetNumber) {
+        userNumber = guessNumberToUser();
+        attempts++;
+        if (userNumber < targetNumber) {
+            console.log("C'est plus !");
+        } else if (userNumber > targetNumber) {
+            console.log("C'est moins !");
+        } else {
+            console.log(`Bravo, vous avez trouvé le nombre mystère en ${attempts} essai(s)`);
+            console.log("\n");
+            endGame();
+        }
+    }
+}
+
+main();
