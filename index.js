6346fe475e21cee77c363551f0087052773d47ee
Yohan Maury <mauryyohan1@gmail.com>
2024-11-28 20:46:06 +0100

    Correct language used (100% english) and have data control to the user prompt

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function getRandomNumber(min, max) {
 
 // Fonction qui demande la saisie d'un nombre à l'utilisateur
 const guessNumberToUser = () => {
-    let userNumber = prompt("Saisir un nombre : ");
-    while (userNumber < 0 || userNumber > 100 || isNaN(userNumber) || userNumber === "") {
-        console.log("Merci d'entrer un nombre valide entre 0 and 100");
+    let userNumber = Number(prompt("Enter a number : "));
+    while (userNumber < 0 || userNumber > 100 || isNaN(userNumber)) {
+        console.log("You have to enter a valid number between 0 and 100");
         userNumber = guessNumberToUser();
     }
     return userNumber;
@@ -47,11 +47,11 @@ function main() {
         userNumber = guessNumberToUser();
         attempts++;
         if (userNumber < targetNumber) {
-            console.log("C'est plus !");
+            console.log("It's higher !");
         } else if (userNumber > targetNumber) {
-            console.log("C'est moins !");
+            console.log("It's small !");
         } else {
-            console.log(`Bravo, vous avez trouvé le nombre mystère en ${attempts} essai(s)`);
+            console.log(`Well done, you have find the mystery number in ${attempts} attempt(s)`);
             console.log("\n");
             endGame();
         }
